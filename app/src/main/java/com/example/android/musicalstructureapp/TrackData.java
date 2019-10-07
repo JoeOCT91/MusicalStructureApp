2b75a2897770a7937eb5a51b5bf8f406398195ff
Yousef Mohamed <unlimitsmar@gmail.com>
2019-10-07 23:45:11 +0200

    neW

--- app/src/main/java/com/example/android/musicalstructureapp/TrackData.java
+++ app/src/main/java/com/example/android/musicalstructureapp/TrackData.java
@@ -8,16 +8,25 @@ public class TrackData {
 
 
     public TrackData(){
-        tracks.add(new Track("Amr diab1", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab2", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab3", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab4", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab5", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab6", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab7", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab8", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab9", "Tamally Maak","ya 7abibey", R.drawable.image1));
-        tracks.add(new Track("Amr diab10", "Tamally Maak","ya 7abibey", R.drawable.image1));
+        tracks.add(new Track("Mohamed Hamaki", "Kol Yom Men Dah","Ya Sattar", R.drawable.kolyoummn));
+        tracks.add(new Track("Mohamed Hamaki", "Kol Yom Men Dah","Mel Bedaya", R.drawable.kolyoummn));
+        tracks.add(new Track("Amr Diab", "El Leila","Garaly Eeh", R.drawable.image1));
+        tracks.add(new Track("Amr Diab", "El Leila","Khalina Lewahdina", R.drawable.ellila));
+        tracks.add(new Track("Amr Diab", "El Leila","Habeet Ya Alby", R.drawable.ellila));
+        tracks.add(new Track("Amr Diab", "Tamally Maak","ya 7abibey", R.drawable.ellila));
+        tracks.add(new Track("Amr Diab", "Tamally Maak","ya 7abibey", R.drawable.image1));
+        tracks.add(new Track("Amr Diab", "Tamally Maak","ya 7abibey", R.drawable.image1));
+        tracks.add(new Track("Amr Diab", "Tamally Maak","ya 7abibey", R.drawable.image1));
+        tracks.add(new Track("Amr Diab", "Tamally Maak","ya 7abibey", R.drawable.image1));
+        tracks.add(new Track("Amr Diab", "Tamally Maak","ya 7abibey", R.drawable.image1));
+        tracks.add(new Track("Mohamed Hamaki", "Kol Yom Men Dah","Oddam El Nas", R.drawable.kolyoummn));
+        tracks.add(new Track("Mohamed Hamaki", "Kol Yom Men Dah","Aalo Annek", R.drawable.kolyoummn));
+        tracks.add(new Track("Mohamed Hamaki", "Kol Yom Men Dah","Kan We Kan", R.drawable.kolyoummn));
+        tracks.add(new Track("Mohamed Hamaki", "Omro Ma Yegheeb","Agmal Youm", R.drawable.omro));
+        tracks.add(new Track("Mohamed Hamaki", "Omro Ma Yegheeb","Mabalash", R.drawable.omro));
+        tracks.add(new Track("Mohamed Hamaki", "Omro Ma Yegheeb","Mayni", R.drawable.omro));
+        tracks.add(new Track("Mohamed Hamaki", "Omro Ma Yegheeb","Kanet Henak", R.drawable.omro));
+        tracks.add(new Track("Mohamed Hamaki", "Omro Ma Yegheeb","Yalli Zaalan", R.drawable.omro));
 
 
 
@@ -25,7 +34,20 @@ public class TrackData {
 
     public  ArrayList<Track> getTracks()
     {
+
         return  tracks;
     }
+
+    public  ArrayList<Track> getAlbums(String albumName)
+    {
+
+
+        return  tracks;
+    }
+
+
+
+
+
 
 }
