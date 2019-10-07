16a5beb8dc20f1411e93143ed1bc71d9cb185944
Yousef Mohamed <unlimitsmar@gmail.com>
2019-10-07 12:39:51 +0200

    neW

--- app/src/main/java/com/example/android/musicalstructureapp/Track.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Track.java
@@ -1,13 +1,16 @@
 package com.example.android.musicalstructureapp;
 
-public class Track {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Track implements Parcelable {
 
     private String mArtistName;
     private String mAlbumName;
     private String mTrackName;
     private int mArtImageId;
 
-    public Track(String artistName, String albumName,String trackName, int artImageId) {
+    public Track(String artistName, String albumName, String trackName, int artImageId) {
 
         mArtistName = artistName;
         mAlbumName = albumName;
@@ -32,10 +35,44 @@ public class Track {
         return mArtImageId;
     }
 
+    protected Track(Parcel in) {
+        mArtistName = in.readString();
+        mAlbumName = in.readString();
+        mTrackName = in.readString();
+        mArtImageId = in.readInt();
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mArtistName);
+        dest.writeString(mAlbumName);
+        dest.writeString(mTrackName);
+        dest.writeInt(mArtImageId);
+
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Track> CREATOR = new Parcelable.Creator<Track>() {
+        @Override
+        public Track createFromParcel(Parcel in) {
+            return new Track(in);
+        }
+
+        @Override
+        public Track[] newArray(int size) {
+            return new Track[size];
+        }
 
 
-
-
+    };
 
 
 }
+
+
